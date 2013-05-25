35144900d82ec725f51013de6cdfe09011dbaf33
rafal.enden <rafal@enden.pl>
2013-05-25 13:55:15 +0200

    Fixed formatting according to coding standards.

--- js/commerce_eraty.js
+++ js/commerce_eraty.js
@@ -2,21 +2,26 @@
 
   Drupal.behaviors.commerce_eraty = {
     attach: function(context, settings) {
-      $('.commerce-eraty-calculate-button a', context).unbind('click').bind('click', function() {
-        Drupal.theme('commerce_eraty_popup', $(this).find('img').attr('title'), $(this).attr('href'));
+      var button = $('#commerce-eraty-calculate-button a', context).unbind('click').bind('click', function() {
+        Drupal.theme(
+          'commerce_eraty_popup',
+          button.find('img').attr('title'),
+          button.attr('href')
+        );
+
         return false;
       });
 
-      $('.commerce-eraty-how-to-buy-button a, .commerce-eraty-rules + label a', context).attr('target', '_blank');
+      $('#commerce-eraty-how-to-buy-button a, #commerce-eraty-rules + label a', context).attr('target', '_blank');
     }
   };
 
   Drupal.theme.prototype.commerce_eraty_popup = function(title, url) {
-    var html = '';
-    html += '<div class="commerce-eraty-popup">';
-    html += '  <iframe src="' + url + '" width="100%" height="99%" />';
-    html += '</div>';
-    $(html).dialog({
+    var popup = '';
+    popup += '<div id="commerce-eraty-popup">';
+    popup += '  <iframe src="' + url + '" width="100%" height="99%" />';
+    popup += '</div>';
+    $(popup).dialog({
       title: title,
       modal: true,
       width: 690,
